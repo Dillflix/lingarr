330442bed2fe3ec1a9a77744c7723953573806e0
Dillflix <116417856+Dillflix@users.noreply.github.com>
2026-08-25 16:32:21 -0700

    Implement reference-free benchmark client calls

--- Lingarr.Client/src/services/resegmentationService.ts
+++ Lingarr.Client/src/services/resegmentationService.ts
@@ -1,5 +1,9 @@
 import { AxiosError, AxiosResponse, AxiosStatic } from 'axios'
-import { IResegmentationEvaluationRequest, IResegmentationService } from '@/ts'
+import {
+    IResegmentationBenchmarkRunRequest,
+    IResegmentationEvaluationRequest,
+    IResegmentationService
+} from '@/ts'
 
 const service = (http: AxiosStatic, resource = '/api/resegmentation'): IResegmentationService => ({
     evaluate<T>(request: IResegmentationEvaluationRequest): Promise<T> {
@@ -11,6 +15,38 @@ const service = (http: AxiosStatic, resource = '/api/resegmentation'): IResegmen
                 .catch((error: AxiosError) => {
                     reject(error.response)
                 })
+        })
+    },
+
+    benchmarkCount(): Promise<number> {
+        return new Promise((resolve, reject) => {
+            http.get(`${resource}/benchmark/count`)
+                .then((response: AxiosResponse<number>) => resolve(response.data))
+                .catch((error: AxiosError) => reject(error.response))
+        })
+    },
+
+    harvestBenchmark<T>(maxRequests = 100): Promise<T> {
+        return new Promise((resolve, reject) => {
+            http.post(`${resource}/benchmark/harvest`, null, { params: { maxRequests } })
+                .then((response: AxiosResponse<T>) => resolve(response.data))
+                .catch((error: AxiosError) => reject(error.response))
+        })
+    },
+
+    runBenchmark<T>(request: IResegmentationBenchmarkRunRequest): Promise<T> {
+        return new Promise((resolve, reject) => {
+            http.post(`${resource}/benchmark/run`, request)
+                .then((response: AxiosResponse<T>) => resolve(response.data))
+                .catch((error: AxiosError) => reject(error.response))
+        })
+    },
+
+    clearBenchmarkSamples(): Promise<number> {
+        return new Promise((resolve, reject) => {
+            http.delete(`${resource}/benchmark/samples`)
+                .then((response: AxiosResponse<number>) => resolve(response.data))
+                .catch((error: AxiosError) => reject(error.response))
         })
     }
 })

d7d9de2b97aaf5281d1708d0fedb56da6a1a95c1
Dillflix <116417856+Dillflix@users.noreply.github.com>
2026-08-25 13:44:25 -0700

    Make paired-context prompt test imports explicit

--- Lingarr.Server.Tests/Services/Translation/BaseLanguageServiceContextTests.cs
+++ Lingarr.Server.Tests/Services/Translation/BaseLanguageServiceContextTests.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Lingarr.Contracts.Models;
 using Lingarr.Contracts.Translation;
 using Lingarr.Server.Interfaces.Services;
